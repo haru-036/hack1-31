8957feee341e4e4f9df1180c3b769a323d2232af
haru <hinagoro5656@gmail.com>
2025-05-31 01:30:54 +0900

    style: biome

--- lib/store.ts
+++ lib/store.ts
@@ -1,12 +1,12 @@
-import type { Data } from '@/app/create/r3fCanvas'
-import { create } from 'zustand'
+import type { Data } from "@/app/create/r3fCanvas";
+import { create } from "zustand";
 
 type ObjectStore = {
-  objectData: Data | null
-  setObjectData: (data: Data) => void
-}
+	objectData: Data | null;
+	setObjectData: (data: Data) => void;
+};
 
 export const useObjectStore = create<ObjectStore>((set) => ({
-  objectData: null,
-  setObjectData: (data) => set({ objectData: data }),
-}))
+	objectData: null,
+	setObjectData: (data) => set({ objectData: data }),
+}));
