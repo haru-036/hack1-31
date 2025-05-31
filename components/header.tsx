076772104f7973c9412c3e2ed1f4799d84dd5393
haru <hinagoro5656@gmail.com>
2025-05-31 21:45:04 +0900

    feat: ヘッダーコンポーネントのアバターサイズを変更し、画像ソースを更新

--- components/header.tsx
+++ components/header.tsx
@@ -5,11 +5,11 @@ export default function Header() {
 		<header className="flex justify-between items-center max-w-7xl lg:container mx-auto px-10 py-3.5">
 			<div className="font-bold">LOGO</div>
 			<div className="flex items-center gap-2">
-				<Avatar className="w-6 h-6">
-					<AvatarImage src="https://github.com/shadcn.png" />
+				<Avatar className="w-7 h-7">
+					<AvatarImage src="https://api.dicebear.com/9.x/fun-emoji/svg" />
 					<AvatarFallback>CN</AvatarFallback>
 				</Avatar>
-				<p className="text-sm text-neutral-700">田中太郎</p>
+				{/* <p className="text-sm text-neutral-700">田中太郎</p> */}
 			</div>
 		</header>
 	);
