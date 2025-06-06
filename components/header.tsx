111768d64b5d0a97130adebbe8e9155d3fcf0e0f
haru <hinagoro5656@gmail.com>
2025-06-07 00:30:41 +0900

    feat: ヘッダーにロゴ画像を追加し、テキストリンクを置き換えました。また、favicon.icoを更新しました。

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,13 @@
+import Image from "next/image";
 import Link from "next/link";
 import UserIcon from "./userIcon";
 
 export default function Header() {
 	return (
 		<header className="flex justify-between items-center max-w-7xl lg:container mx-auto px-10 py-3.5">
-			<div className="font-black text-lg">
-				<Link href="/">BuildCha</Link>
-			</div>
+			<Link href="/" className="h-7 w-auto block">
+				<Image src="/logo.png" alt="logo" width={90} height={28} />
+			</Link>
 			<div className="flex items-center gap-2">
 				<UserIcon />
 				{/* <p className="text-sm text-neutral-700">田中太郎</p> */}
