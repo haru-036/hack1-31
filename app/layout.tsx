62c0d1cc60c1f1c0c02d403c7dc200808d62e586
haru <hinagoro5656@gmail.com>
2025-06-08 23:23:09 +0900

    feat: メタデータを統一し、Open Graph情報を追加しました。

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AppWrapper from "@/components/appWrapper";
@@ -14,7 +13,7 @@ const geistMono = Geist_Mono({
 	subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
+export const metadata = {
 	title: "BuildCha",
 	description: "話して、作ろう。自分だけのまち。",
 	openGraph: {
